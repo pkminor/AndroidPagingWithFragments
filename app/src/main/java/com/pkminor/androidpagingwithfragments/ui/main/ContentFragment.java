e1dc6a3f3de36d4da5009b735b610dd5ea5e5d4c
pkminor <minorpk@gmail.com>
2020-09-19 16:59:18 +0300

    initial commit

---
+++ app/src/main/java/com/pkminor/androidpagingwithfragments/ui/main/ContentFragment.java
@@ -0,0 +1,89 @@
+package com.pkminor.androidpagingwithfragments.ui.main;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.pkminor.androidpagingwithfragments.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ContentFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ContentFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private static Context context;
+
+    public ContentFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ContentStaffFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ContentFragment newInstance(Context c, String param1, String param2) {
+        ContentFragment fragment = new ContentFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        context=c;
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_content, container, false);
+        BottomNavigationView navView = view.findViewById(R.id.nav_view);
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications)
+                .build();
+
+        NavController navController = Navigation.findNavController(view.findViewById(R.id.nav_host_fragment));
+//        NavigationUI.setupActionBarWithNavController((AppCompatActivity) getActivity(), navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+        return view;
+    }
+}
